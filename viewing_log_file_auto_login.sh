009c049370fa778d5c24e3e02ff93362fc78728c
kripton20 <kripton20@mail.ru>
2025-09-15 14:20:15 +0300

    Удаление лог-файла в зависимости от шестого параметра командной строки. Добавил команду в comands_control_auto_login.txt Исправление в файле viewing_log_file_auto_login.sh

--- viewing_log_file_auto_login.sh
+++ viewing_log_file_auto_login.sh
@@ -15,6 +15,6 @@ echo "Лог-файл с выборкой строк."
 grep -E "starting" /home/vmail/auto_login/logs/auto_multi_login.log && echo
 grep -E "exception" /home/vmail/auto_login/logs/auto_multi_login.log && echo
 grep -E "stopping" /home/vmail/auto_login/logs/auto_multi_login.log && echo
- 
+
 # Как запускать.
-# sh /home/vmail/auto_login/viewingLogFiles.sh & sh /home/vmail/rc147/viewingLogFiles.sh
+# sh /home/vmail/auto_login/logs/auto_multi_login.log
