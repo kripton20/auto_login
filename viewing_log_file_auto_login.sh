1c60dd16476cf97520dc434defb40377eeeae4ad
kripton20 <kripton20@mail.ru>
2025-09-15 09:24:17 +0300

    Добавил удаление лог-файла перед каждым запуском. Оставил выборку строк.

--- viewing_log_file_auto_login.sh
+++ viewing_log_file_auto_login.sh
@@ -2,18 +2,19 @@
 # Смотрим лог работы прожения auto_multi_login.
 echo
 
+# Полный вывод содержимого файла auto_multi_login.log.
+#cat /home/vmail/auto_login/logs/auto_multi_login.log
+
 # Последние 20 строк строк.
 echo "Последние 20 строк строк."
 tail -n20 /home/vmail/auto_login/logs/auto_multi_login.log
 echo
 
 # Лог-файл с выборкой строк.
-#echo "Лог-файл с выборкой строк."
-#grep -E "starting" /home/vmail/auto_login/logs/auto_multi_login.log && echo
-#grep -E "exception" /home/vmail/auto_login/logs/auto_multi_login.log && echo
-#grep -E "stopping" /home/vmail/auto_login/logs/auto_multi_login.log && echo
+echo "Лог-файл с выборкой строк."
+grep -E "starting" /home/vmail/auto_login/logs/auto_multi_login.log && echo
+grep -E "exception" /home/vmail/auto_login/logs/auto_multi_login.log && echo
+grep -E "stopping" /home/vmail/auto_login/logs/auto_multi_login.log && echo
  
-# clear && sh /var/www/viewing_log_files.sh | # grep -E "" /var/log/rm_duplicate_messages/full_rm_duplicate.log | # grep -E "2018_1\s" && echo && # | sort -k7
-
 # Как запускать.
 # sh /home/vmail/auto_login/viewingLogFiles.sh & sh /home/vmail/rc147/viewingLogFiles.sh
