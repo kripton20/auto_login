53202683b4a357d91fa3288f7a5c13f5f39bd6cd
kripton20 <kripton20@mail.ru>
2025-10-03 17:03:27 +0300

    Мелкие доработки

--- viewing_log_file_auto_login.sh
+++ viewing_log_file_auto_login.sh
@@ -13,8 +13,11 @@ echo
 # Лог-файл с выборкой строк.
 echo "Лог-файл с выборкой строк."
 grep -E "starting" /home/vmail/auto_login/logs/auto_multi_login.log && echo
-grep -E "exception" /home/vmail/auto_login/logs/auto_multi_login.log && echo
 grep -E "stopping" /home/vmail/auto_login/logs/auto_multi_login.log && echo
+
+# Освободившиеся URLs.
+echo "Освободившиеся URLs."
+grep -E "exception" /home/vmail/auto_login/logs/auto_multi_login.log && echo
 
 # Как запускать.
 # sh /home/vmail/auto_login/logs/auto_multi_login.log
